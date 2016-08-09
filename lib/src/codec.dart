9661a8ef54b67eb21263c99d6d5a101a04348cd2
Ravi Teja Gudapati <tejainece@gmail.com>
2016-08-09 23:52:59 +0200

    Fixed view to model

--- lib/src/codec.dart
+++ lib/src/codec.dart
@@ -2,12 +2,17 @@ part of redstone_mongo.src;
 
 Object decoder(
     Object data, String fieldName, mapper.Field fieldInfo, List metadata) {
-  String name = fieldInfo.model;
-  if (name == null) {
-    name = fieldName;
+  String lName = fieldName;
+
+  if (fieldInfo.model is String) {
+    if (fieldInfo.model.isEmpty) {
+      return mapper.ignoreValue;
+    }
+
+    lName = fieldInfo.model;
   }
 
-  var value = (data as Map)[name];
+  var value = (data as Map)[lName];
 
   if (fieldInfo is Id || fieldInfo is RefId) {
     if (value is mgo.ObjectId) {
@@ -22,22 +27,28 @@ Object decoder(
 
 void encoder(Map data, String fieldName, mapper.Field fieldInfo,
     List metadata, Object value) {
-  String name = fieldInfo.model;
-  if (name == null) {
-    name = fieldName;
+  String lName = fieldName;
+
+  if (fieldInfo.model is String) {
+    if (fieldInfo.model.isEmpty) {
+      return;
+    }
+
+    lName = fieldInfo.model;
   }
+
   if (fieldInfo is Id || fieldInfo is RefId) {
     if (value != null) {
       if (value is String) {
         value = mgo.ObjectId.parse(value);
-        data[name] = value;
+        data[lName] = value;
       } else if (value is List) {
         value = (value as List).map((o) => mgo.ObjectId.parse(o)).toList();
-        data[name] = value;
+        data[lName] = value;
       }
     }
   } else {
-    data[name] = value;
+    data[lName] = value;
   }
 }
 
